a3d96bb1ae99b53370104334d80054cd7d1a6054
Roman Zotov <zotov_r_g@mail.ru>
2021-11-24 01:33:58 +0300

    initial commit
    Adding site files

---
+++ weblayout-itost/js/app.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+
+  //AutoplayOptions 
+  autoplay: {
+    delay: 5000,
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
