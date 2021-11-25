0c2a27d233ce866283df3e675b9ef14d14a1e55b
Roman Zotov <zotov_r_g@mail.ru>
2021-11-25 23:56:05 +0300

    Implementation of interactivity
    Implementing interactivity for page elements

--- weblayout-itost/js/app.js
+++ weblayout-itost/js/app.js
@@ -1,3 +1,9 @@
+//preloader
+
+
+
+
+//swiper
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
@@ -18,4 +24,36 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 
+  //скорость прокрутки
+  speed: 1000,
+
 });
+
+//burger menu
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('#box-on').addEventListener('click', function () {
+    document.querySelector('#contact-box').classList.add('header-contacts__box-is-active');
+  });
+  document.querySelector('#contact-box').addEventListener('click', function () {
+    document.querySelector('#contact-box').classList.remove('header-contacts__box-is-active');
+  });
+});
+
+// появление элементов при скроле
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+//плавное появление hero__container
+$('.hero__container').delay(1000).animate({'opacity':'1'},500);
